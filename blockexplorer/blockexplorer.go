29626b5aceabec3583e5e2c000a3579fb8d91a2f
Ben Kremer <ben.kremer@hotmail.co.uk>
2017-03-08 21:15:35 +0100

    Removes URLPattern reference

--- blockexplorer/blockexplorer.go
+++ blockexplorer/blockexplorer.go
@@ -7,21 +7,6 @@ import (
 	"github.com/bkrem/blockchain.info-api-v1-client-go/util"
 )
 
-/*
-module.exports = {
-  rawblock: new UrlPattern('/rawblock/:hash(?api_code=:apiCode)'),
-  rawtx: new UrlPattern('/rawtx/:hash(?api_code=:apiCode)'),
-  blockHeight: new UrlPattern('/block-height/:height?format=json(&api_code=:apiCode)'),
-  address: new UrlPattern('/address/:address?format=json(&limit=:limit)(&offset=:offset)(&api_code=:apiCode)'),
-  multiaddr: new UrlPattern('/multiaddr?active=:active(&n=:limit)(&offset=:offset)(&api_code=:apiCode)'),
-  unspent: new UrlPattern('/unspent?active=:active(&api_code=:apiCode)'),
-  latestblock: new UrlPattern('/latestblock(?api_code=:apiCode)'),
-  unconfTxs: new UrlPattern('/unconfirmed-transactions?format=json(&api_code=:apiCode)'),
-  blocks: new UrlPattern('/blocks/:time?format=json(&api_code=:apiCode)'),
-  inv: new UrlPattern('/inv/:hash?format=json(&api_code=:apiCode)')
-}
-*/
-
 type blockExplorerOpts struct {
 	api.Opts
 	Limit  int    `url:"limit,omitempty"`
